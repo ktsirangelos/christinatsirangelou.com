0d9efa3d648ba5c238a891e7e3cffa591b3da881
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-01-14 15:32:55 +0100

    Added public page html

--- src/pages/public/Public.tsx
+++ src/pages/public/Public.tsx
@@ -1,5 +1,21 @@
 const Public = () => {
-  return <h1>Public</h1>;
+  return (
+    <main className="public-main">
+      <section className="header-text">
+        A list of all recent News, Publications and Events
+      </section>
+      <section className="public-section">
+        <div className="public-template">
+          <p>Title</p>
+          <p></p>
+          <p>Type</p>
+          <p></p>
+          <p>Year</p>
+        </div>
+        <div className="publications"></div>
+      </section>
+    </main>
+  );
 };
 
 export { Public };
