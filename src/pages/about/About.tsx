e214569fdf3516dc7ed81f4a62b56a0d50bf7771
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-03-19 19:57:54 +0200

    About scaffold

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -1,5 +1,75 @@
-const About = () => {
-  return <h1>About</h1>;
+import { clients } from "./About.data";
+import "./About.scss";
+
+const AboutHeader = () => {
+  return (
+    <header className="about-header">
+      A list of all recent News, Publications and Events
+    </header>
+  );
 };
 
-export { About };
+const AboutText = () => {
+  return (
+    <div className="about-text">
+      <p className="first-paragraph">
+        Christina Tsirangelou, as an MA architect, in 2009, co-founded 157+173
+        Designers, an independent Product design studio with a global reach,
+        gaining attention through media and trade fairs.
+      </p>
+      <p className="second-paragraph">
+        During 2021- 2023, she became an Associate Creative director & Partner
+        at Urban Soul Project (USP), a leading Greek architectural firm
+        specializing in Hotel, Retail, Offices and Residential projects. This
+        role enriched her expertise in large-scale projects, addressing
+        challenges arising from the firm's rapid expansion to 100+ architects by
+        2023.
+      </p>
+      <p className="thrid-paragraph">
+        In the academic realm, in 2021, she assumed a role as an assistant
+        teacher at the Aristotle University of Thessaloniki in Greece,
+        contributing to the Bachelor's program in Interior Architectural Design.
+      </p>
+      <p className="fourth-paragraph">
+        She deeply values the experience she gained collaborating with the
+        architectural practice of Giorgos Triantafilidis, Arch. Prof. of UTH.
+        Also, co-teaching in the BA Interior Architecture of AKTO+Middlesex
+        Univestity, by the side of Arch. Prof. Dionisis Tsasis. Subsequently
+        increased her experience in large scale projects collaborating with
+        Sparch Architects, Arch. Prof. Rena Sakelaridou & Morpho Papanikolaou
+        from 2006-2009.
+      </p>
+    </div>
+  );
+};
+
+const AboutClients = () => {
+  return (
+    <div className="clients-list">
+      {clients.map(({ name, url }, id) => (
+        <div className="" key={id}>
+          <p>{name}</p>
+          <p>{url}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const AboutMain = () => {
+  return (
+    <main className="about-main">
+      <AboutText />
+      <AboutClients />
+    </main>
+  );
+};
+
+export const About = () => {
+  return (
+    <>
+      <AboutHeader />
+      <AboutMain />
+    </>
+  );
+};
