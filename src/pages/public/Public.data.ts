ff5dfbc632e4bfa180a761576ae91b200fea6604
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-03-25 22:06:56 +0200

    Fix typo

--- src/pages/public/Public.data.ts
+++ src/pages/public/Public.data.ts
@@ -12,14 +12,14 @@ export const publications = [
   {
     id: 1,
     title: "Social Living Greece",
-    url: "https://www.linkedin.com/company/social-living-greece/",
+    url: "https://linkedin.com/company/social-living-greece/",
     type: `${PublicationCategory.Architecture}, ${PublicationCategory.Research}`,
     year: 2024,
   },
   {
     id: 2,
     title: "a37.studio",
-    url: "https:a37.studio",
+    url: "https://a37.studio",
     type: `${PublicationCategory.Architecture}, ${PublicationCategory.Collaboration}`,
     year: 2024,
   },
