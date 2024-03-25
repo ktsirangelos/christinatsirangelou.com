fc117c618a68b46fc4827303dbd254bbbccfc35f
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-03-25 21:13:17 +0200

    Refactor Work page and data

--- src/pages/work/Work.tsx
+++ src/pages/work/Work.tsx
@@ -1,9 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, memo } from "react";
 import { Accordion, AccordionItem } from "@szhsin/react-accordion";
 import { Link } from "react-router-dom";
 import { projects, Project } from "./Work.data";
 import "./Work.scss";
 import { useState } from "react";
+
+type ProjectCategoriesProps = {
+  filterProjectsByCategory: (category: string) => void;
+};
+
+type ProjectsAccordionProps = {
+  projects: Project[];
+  selectedCategory: string;
+};
 
 const WorkHeader = () => {
   return (
@@ -15,130 +24,102 @@ const WorkHeader = () => {
   );
 };
 
-type ProjectCategoriesProps = {
-  filterProjectsByCategory: (category: string) => void;
-};
+const categories = [
+  "all",
+  "architecture",
+  "interior",
+  "product",
+  "research",
+  "exhibition",
+  "education",
+];
 
-const ProjectCategories = ({
-  filterProjectsByCategory,
-}: ProjectCategoriesProps) => {
-  return (
+const ProjectCategories = memo(
+  ({ filterProjectsByCategory }: ProjectCategoriesProps) => (
     <ul className="project-categories">
-      <li className="all">
-        <button onClick={() => filterProjectsByCategory("all")}>All</button>
-      </li>
-      <li className="arc">
-        <button onClick={() => filterProjectsByCategory("architecture")}>
-          Architecture
-        </button>
-      </li>
-      <li className="int">
-        <button onClick={() => filterProjectsByCategory("interior")}>
-          Interior
-        </button>
-      </li>
-      <li className="pro">
-        <button onClick={() => filterProjectsByCategory("product")}>
-          Product
-        </button>
-      </li>
-      <li className="res">
-        <button onClick={() => filterProjectsByCategory("research")}>
-          Researh
-        </button>
-      </li>
-      <li className="exh">
-        <button onClick={() => filterProjectsByCategory("exhibition")}>
-          Exhibition
-        </button>
-      </li>
-      <li className="edu">
-        <button onClick={() => filterProjectsByCategory("education")}>
-          Education
-        </button>
-      </li>
+      {categories.map((category) => (
+        <li key={category} className={category.slice(0, 3)}>
+          <button onClick={() => filterProjectsByCategory(category)}>
+            {category.charAt(0).toUpperCase() + category.slice(1)}
+          </button>
+        </li>
+      ))}
     </ul>
-  );
-};
+  ),
+);
 
-type ProjectsAccordionProps = {
-  projects: Project[];
-  selectedCategory: string;
-};
-
-const ProjectsAccordion = ({
-  projects,
-  selectedCategory,
-}: ProjectsAccordionProps) => {
-  return (
-    <Accordion>
-      {projects.map(
-        (
-          {
-            title,
-            type,
-            year,
-            location,
-            client,
-            status,
-            size,
-            category,
-            pictureURLs,
-          },
-          id,
-        ) => (
-          <AccordionItem
-            header={
-              <>
-                <div className="title">{title}</div>
-                <div className="type">{type}</div>
-                <div className="year">{year}</div>
-              </>
-            }
-            key={id}
-            className={
-              selectedCategory !== "all" && category !== selectedCategory
-                ? "inactive"
-                : ""
-            }
-          >
-            <div className="accordion-item-container">
-              {pictureURLs?.map((img, index) => (
-                <Link
-                  className={`project-image-${index + 1}`}
-                  to={`/work/${title}`}
-                  key={index}
-                >
-                  <img
-                    src={img}
-                    alt={`${title} Image ${index + 1}`}
+const ProjectsAccordion = memo(
+  ({ projects, selectedCategory }: ProjectsAccordionProps) => {
+    return (
+      <Accordion>
+        {projects.map(
+          (
+            {
+              title,
+              type,
+              year,
+              location,
+              client,
+              status,
+              size,
+              category,
+              pictureURLs,
+            },
+            id,
+          ) => (
+            <AccordionItem
+              header={
+                <>
+                  <div className="title">{title}</div>
+                  <div className="type">{type}</div>
+                  <div className="year">{year}</div>
+                </>
+              }
+              key={id}
+              className={
+                selectedCategory !== "all" && category !== selectedCategory
+                  ? "inactive"
+                  : ""
+              }
+            >
+              <div className="accordion-item-container">
+                {pictureURLs?.map((img, index) => (
+                  <Link
+                    className={`project-image-${index + 1}`}
+                    to={`/work/${title}`}
                     key={index}
-                  />
-                </Link>
-              ))}
-              <div className="description">
-                <div className="details">
-                  <div className="location">Location: {location}</div>
-                  <div className="client">Client: {client}</div>
-                  <div className="status">Status: {status}</div>
-                  <div className="size">Size: {size} sqm</div>
-                </div>
-                <div className="explore">
-                  <Link to={`/work/${title}`} key={`${title}-explore`}>
-                    Explore
-                    <span className="material-symbols-outlined">
-                      arrow_outward
-                    </span>
+                  >
+                    <img
+                      src={img}
+                      alt={`${title} Image ${index + 1}`}
+                      key={index}
+                    />
                   </Link>
+                ))}
+                <div className="description">
+                  <div className="details">
+                    <div className="location">Location: {location}</div>
+                    <div className="client">Client: {client}</div>
+                    <div className="status">Status: {status}</div>
+                    <div className="size">Size: {size} sqm</div>
+                  </div>
+                  <div className="explore">
+                    <Link to={`/work/${title}`} key={`${title}-explore`}>
+                      Explore
+                      <span className="material-symbols-outlined">
+                        arrow_outward
+                      </span>
+                    </Link>
+                  </div>
                 </div>
               </div>
-            </div>
-          </AccordionItem>
-        ),
-      )}
-    </Accordion>
-  );
-};
+            </AccordionItem>
+          ),
+        )}
+      </Accordion>
+    );
+  },
+);
 
 const WorkMain = () => {
   const [selectedCategory, setSelectedCategory] = useState("all");
