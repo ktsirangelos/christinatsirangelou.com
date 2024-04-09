7aea0b1a0b0233a620c70c9aa99c00681c02abd4
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-04-09 18:13:22 +0300

    Correction

--- src/pages/404/NotFound.tsx
+++ src/pages/404/NotFound.tsx
@@ -6,7 +6,7 @@ export const PageNotFound = () => {
     <div className="container">
       <h1>Oh oh! Page Not Found :/</h1>
       <NavLink to="/">
-        <h2 className="link">Okay, take me back Home:]</h2>
+        <h2 className="link">Okay, take me back Home :]</h2>
       </NavLink>
     </div>
   );
@@ -17,7 +17,7 @@ export const ProjectNotFound = () => {
     <div className="container">
       <h1>Oh oh! Project Not Found :/</h1>
       <NavLink to="/work">
-        <h2 className="link">Okay, take me back to Work:]</h2>
+        <h2 className="link">Okay, take me back to Work :]</h2>
       </NavLink>
     </div>
   );
