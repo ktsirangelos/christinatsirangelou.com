6dce6cc9eccb0296d946ae994c4de1c6d0c28421
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-04-11 14:07:31 +0300

    Add scrolling to top when changing routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import { RoutesComponent as Routes } from "./components/RoutesComponent";
+import { ScrollToTop } from "@/utils/ScrollToTop";
 import "./scss/main.scss";
 
 export const App = () => {
   return (
     <Router>
+      <ScrollToTop />
       <Routes />
     </Router>
   );
