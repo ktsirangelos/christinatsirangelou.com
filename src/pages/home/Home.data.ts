5e37f29895f0d5dc2b3af72df2bb8d83dc150144
Konstantinos Tsirangelos <ktsirangelos@proton.me>
2024-04-22 14:48:15 +0300

    Add responsive pictures

--- src/pages/home/Home.data.ts
+++ src/pages/home/Home.data.ts
@@ -1,4 +1,5 @@
-import landing1 from "@/pictures/home/Corfu_5star hotel1.webp";
+import landing1_mobile from "@/pictures/home/Corfu_5star_hotel_mobile.webp";
+import landing1_desktop from "@/pictures/home/Corfu_5star_hotel_desktop.webp";
 import landing2 from "@/pictures/home/DSC_0030 copy 2.webp";
 import landing3 from "@/pictures/home/L0_1102_USP_Linving_Ap_Cam001 copy.webp";
 import landing4 from "@/pictures/home/01-157173_pizzapls_y1440.webp";
@@ -12,8 +13,8 @@ import landing10 from "@/pictures/home/Thessaloniki_Feta Bar2.webp";
 export const landingPictures = [
   {
     id: 1,
-    srcMobile: landing1,
-    srcDesktop: landing1,
+    srcMobile: landing1_mobile,
+    srcDesktop: landing1_desktop,
   },
   {
     id: 2,
